8e06c7dc24acd0618e228a6a49fbd72f7bcf60d6
tanjiexi <229008199@qq.com>
2018-04-13 12:00:03 +0800

    2222

--- app/src/main/java/com/example/stu/rxjavademo/MainActivity.java
+++ app/src/main/java/com/example/stu/rxjavademo/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnOne(View view) {
         //1.2
+        //5.6
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> e) throws Exception {
