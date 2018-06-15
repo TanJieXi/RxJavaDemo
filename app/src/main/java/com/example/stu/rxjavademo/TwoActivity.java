228ee35d186404a51568be856dfb2563349af361
tanjiexi <229008199@qq.com>
2018-06-15 08:53:26 +0800

    11111111111111111

--- app/src/main/java/com/example/stu/rxjavademo/TwoActivity.java
+++ app/src/main/java/com/example/stu/rxjavademo/TwoActivity.java
@@ -1,9 +1,10 @@
 package com.example.stu.rxjavademo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,7 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 
-public class TwoActivity extends AppCompatActivity {
+public class TwoActivity extends RxAppCompatActivity {
     private final String TAG = "dsfdsafgsdf";
 
     @Override
@@ -27,6 +28,7 @@ public class TwoActivity extends AppCompatActivity {
     }
 
     public void btnOne(View view) {
+        /*.compose(this.<Integer>bindToLifecycle())*/
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
@@ -40,7 +42,7 @@ public class TwoActivity extends AppCompatActivity {
         }).map(new Function<Integer, String>() {
             @Override
             public String apply(Integer integer) throws Exception {
-                return integer+"你好";
+                return integer + "你好";
             }
         }).subscribe(new Observer<String>() {
             @Override
@@ -75,22 +77,25 @@ public class TwoActivity extends AppCompatActivity {
                 e.onNext(4);
             }
             //2. 使用Map变换操作符中的Function函数对被观察者发送的事件进行统一变换：整型变换成字符串类型
-        }).flatMap(new Function<Integer, ObservableSource<String>>() {
-            @Override
-            public ObservableSource<String> apply(Integer integer) throws Exception {
-                final List<String> list = new ArrayList<>();
-                for (int i = 0; i < 3; i++) {
-                    list.add("我是事件 " + integer + "拆分后的子事件" + i);
-                    // 通过flatMap中将被观察者生产的事件序列先进行拆分，再将每个事件转换为一个新的发送三个String事件
-                    // 最终合并，再发送给被观察者
-                }
-                return Observable.fromIterable(list);
-            }
-        }).subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Exception {
-                Log.i(TAG, "accept--s->" + s);
-            }
-        });;
+        })
+                .flatMap(new Function<Integer, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(Integer integer) throws Exception {
+                        final List<String> list = new ArrayList<>();
+                        for (int i = 0; i < 3; i++) {
+                            list.add("我是事件 " + integer + "拆分后的子事件" + i);
+                            // 通过flatMap中将被观察者生产的事件序列先进行拆分，再将每个事件转换为一个新的发送三个String事件
+                            // 最终合并，再发送给被观察者
+                        }
+                        return Observable.fromIterable(list);
+                    }
+                }).subscribe(new Consumer<String>() {
+                                 @Override
+                                 public void accept(String s) throws Exception {
+                                     Log.i(TAG, "accept--s->" + s);
+                                 }
+                             }
+        );
+        ;
     }
 }
